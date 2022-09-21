75d515be07880733cb6970c354fba1f3a44e9504
devAbreu <yolfrank.abreu@gmail.com>
2022-09-21 18:33:26 -0500

    :hammer: modified class

--- src/models.py
+++ src/models.py
@@ -1,37 +1,30 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 from eralchemy import render_er
 
-Base = declarative_base()
+db = SQLAlchemy()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Person(db.Model):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Address(db.Model):
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = db.Column(db.Integer, primary_key=True)
+    street_name = db.Column(db.String(250))
+    street_number = db.Column(db.String(250))
+    post_code = db.Column(db.String(250), nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    person = db.relationship(Person)
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
 try:
-    result = render_er(Base, 'diagram.png')
+    result = render_er(db.Model, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
     print("There was a problem genering the diagram")
